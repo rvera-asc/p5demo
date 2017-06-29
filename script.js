88ab93befa6dceeda7e64b969aedd978afbe926c
rogerdavidvera <roger.vera@outlook.com>
2017-06-29 15:06:17 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function setup() {
+    createCanvas(600,600)
+}
+
+var x = 300
+var y = 300
+var radius = 50
+
+function draw() {
+    background('#faf')
+    stroke(0);
+    fill(0);
+    ellipse(mouseX,mouseY,radius)
+
+}
